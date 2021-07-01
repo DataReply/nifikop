0397ed7d711cc939546223b2a0760564a98a4b3b
Jon Doe <t.krause@reply.de>
2021-07-01 12:54:29 +0200

    added PR113

--- pkg/clientwrappers/dataflow/dataflow.go
+++ pkg/clientwrappers/dataflow/dataflow.go
@@ -1,9 +1,10 @@
 package dataflow
 
 import (
+	"strings"
+
 	"github.com/Orange-OpenSource/nifikop/pkg/common"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"strings"
 
 	"github.com/Orange-OpenSource/nifikop/api/v1alpha1"
 	"github.com/Orange-OpenSource/nifikop/pkg/clientwrappers"
@@ -71,7 +72,7 @@ func CreateDataflow(
 	updateProcessGroupEntity(flow, registry, cluster, &scratchEntity)
 
 	entity, err := nClient.CreateProcessGroup(scratchEntity, flow.Spec.GetParentProcessGroupID(cluster))
-	if err := clientwrappers.ErrorCreateOperation(log, err, "Create registry-client"); err != nil {
+	if err := clientwrappers.ErrorCreateOperation(log, err, "Create process-group"); err != nil {
 		return nil, err
 	}
 
@@ -695,6 +696,8 @@ func updateProcessGroupEntity(
 	cluster *v1alpha1.NifiCluster,
 	entity *nigoapi.ProcessGroupEntity) {
 
+	stringFactory := func() string { return "" }
+
 	var defaultVersion int64 = 0
 	if entity == nil {
 		entity = &nigoapi.ProcessGroupEntity{}
@@ -718,10 +721,17 @@ func updateProcessGroupEntity(
 	entity.Component.Name = flow.Name
 	entity.Component.ParentGroupId = flow.Spec.GetParentProcessGroupID(cluster)
 	entity.Component.VersionControlInformation = &nigoapi.VersionControlInformationDto{
-		RegistryId: registry.Status.Id,
-		BucketId:   flow.Spec.BucketId,
-		FlowId:     flow.Spec.FlowId,
-		Version:    *flow.Spec.FlowVersion,
+		GroupId:          stringFactory(),
+		RegistryName:     stringFactory(),
+		BucketName:       stringFactory(),
+		FlowName:         stringFactory(),
+		FlowDescription:  stringFactory(),
+		State:            stringFactory(),
+		StateExplanation: stringFactory(),
+		RegistryId:       registry.Status.Id,
+		BucketId:         flow.Spec.BucketId,
+		FlowId:           flow.Spec.FlowId,
+		Version:          *flow.Spec.FlowVersion,
 	}
 }
 
